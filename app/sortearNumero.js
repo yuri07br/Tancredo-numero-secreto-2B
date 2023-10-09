b69f7241fc6dc6d632e108c242d1c9a3e8264eab
YURI TRIQUES DOS SANTOS <106687697+yuri07br@users.noreply.github.com>
2023-10-09 10:47:07 -0300

    Add files via upload

---
+++ app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor = 1;
+const maiorValor = 1000;
+
+const numeroSecreto =  gerarNumeroSecreto()
+
+function gerarNumeroSecreto() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log('Número Secreto:', numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor') 
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
